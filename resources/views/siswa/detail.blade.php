420dd1d0c8200ae5443aa3d1b46e07de8c5ca65d
@Ardyan172 <ardyanrhm321@gmail.com>
2021-06-02 10:40:55 +0700

    mengubah tampilan card di detail siswa

--- resources/views/siswa/detail.blade.php
+++ resources/views/siswa/detail.blade.php
@@ -4,18 +4,26 @@
 
 @section('konten')
 <!-- konten -->
-<div class="row mb-2 text-center">
+<div class="row mb-3">
 	<div class="offset-md-3 col-md-6">
 		<div class="card">
-			<img src="{{ asset('fotoSiswa') }}/{{ $detailSiswa->foto }}" class="card-img-top" alt="foto {{ $detailSiswa->nama }} ">
-			<ul class="list-group list-group-flush">
-				<li class="list-group-item">No absen : {{ $detailSiswa->noAbsen }}</li>
-				<li class="list-group-item">Nama : {{ $detailSiswa->nama }}</li>
-				<li class="list-group-item">Kelas : {{ $detailSiswa->kelas }}</li>
-			</ul>
-			<div class="card-body">
-				<a href="{{ route('siswa') }}" class="btn btn-danger col-md-12">Kembali</a>
-			</div>
+		  <div class="row g-0">
+		    <div class="col-md-4">
+		      <img src="{{ asset('fotoSiswa') }}/{{ $detailSiswa->foto }}" alt="foto {{ $detailSiswa->nama }}">
+		    </div>
+		    <div class="col-md-8">
+		      <div class="card-body">
+		        <h5 class="card-title">Detail Siswa</h5>
+		        <p class="card-text">No absen : {{ $detailSiswa->noAbsen }}</p>
+		        <p class="card-text">Nama : {{ $detailSiswa->nama }}</p>
+		        <p class="card-text">Kelas : {{ $detailSiswa->kelas }}</p>
+		        <p class="card-text"><small class="text-muted">Terakhir di update 3 minutes yang lalu</small></p>
+		      </div>
+		      <div class="card-footer">
+		      	<a href="{{ route('siswa') }}" class="btn btn-warning col-md-12">Kembali</a>
+		      </div>
+		    </div>
+		  </div>
 		</div>
 	</div>
 </div>
