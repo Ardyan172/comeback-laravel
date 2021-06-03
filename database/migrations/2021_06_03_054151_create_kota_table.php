f29106964ff1aa6594eb5175e88276bb8d98bf15
@Ardyan172 <ardyanrhm321@gmail.com>
2021-06-03 16:11:20 +0700

    membuat table pengguna dan table kota

---
+++ database/migrations/2021_06_03_054151_create_kota_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateKotaTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('kota', function (Blueprint $table) {
+            $table->id();
+            $table->integer('id_kota');
+            $table->string('kota');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('kota');
+    }
+}
